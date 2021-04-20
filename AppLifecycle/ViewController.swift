37517279465b3a437d34873711547bfa857f9785
Swmnx <No>
2021-04-20 21:48:19 +0300

    Initial Commit

---
+++ AppLifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifecycle
+//
+//  Created by swamnx on 20.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
